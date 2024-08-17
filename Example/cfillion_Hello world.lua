a726fd3622512b03584f906fadf3b7e93cafdcb8
inversiveaudio <inversiveaudio@gmail.com>
2024-08-17 20:12:21 +0200

    Update cfillion_Hello world.lua

--- Example/cfillion_Hello world.lua
+++ Example/cfillion_Hello world.lua
@@ -5,23 +5,23 @@
 @link https://github.com
 @donation https://github.com
 @about
-
-This is an example of a package file. It installs itself as a ReaScript that
-does nothing but show "Hello World!" in REAPER's scripting console.
-
-Packages may also include additional files specified using the @provides tag.
-
-This text is the documentation shown when using ReaPack's "About this package"
-feature. [Markdown](https://commonmark.org/) *formatting* is supported.
-
-Header Line
-
-Hello *World*
-[This is a link](https://google.com)
- 
-About:
-   - Bullet list item 1
-   - Bullet list item 2
+  #Test
+  This is an example of a package file. It installs itself as a ReaScript that
+  does nothing but show "Hello World!" in REAPER's scripting console.
+  
+  Packages may also include additional files specified using the @provides tag.
+  
+  This text is the documentation shown when using ReaPack's "About this package"
+  feature. [Markdown](https://commonmark.org/) *formatting* is supported.
+  
+  Header Line
+  
+  Hello *World*
+  [This is a link](https://google.com)
+   
+  About:
+     - Bullet list item 1
+     - Bullet list item 2
 
 ------------------------------------------------------------------------------------]]
 
